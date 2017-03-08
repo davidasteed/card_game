6b7557476bf319bbf9e034e900d84d83bc666693
David Steed <davidasteed@gmail.com>
2017-03-07 19:58:22 -0500

    clean up redundant code and inefficient functionality per instructor advice.  looks pretty fab and if time permits...ready to go for adventurer mode

--- src/deck.js
+++ src/deck.js
@@ -1,28 +1,31 @@
-
 module.exports = function buildDeck() {
-  // create an array and populate with cards
-  let newDeck = ["2s", "2c", "2h", "2d",
-                 "3s", "3c", "3h", "3d",
-                 "4s", "4c", "4h", "4d",
-                 "5s", "5c", "5h", "5d",
-                 "6s", "6c", "6h", "6d",
-                 "7s", "7c", "7h", "7d",
-                 "8s", "8c", "8h", "8d",
-                 "9s", "9c", "9h", "9d",
-                 "10s", "10c", "10h", "10d",
-                 "Js", "Jc", "Jh", "Jd",
-                 "Qs", "Qc", "Qh", "Qd",
-                 "Ks", "Kc", "Kh", "Kd",
-                 "As", "Ac", "Ah", "Ad"
-                ];
+  // create a new unshuffled check
+  let newDeck = [];
+  let suit = ["s", "c", "h", "d"];
 
-  // shuffle the decks, using "swap" method borrowed from internet
-  let random_Number, temp;
+  suit.forEach(function pushCards(thisSuit){
+    for (let value = 2; value < 15; value++) {
+      if (value < 11) {
+        newDeck.push(value + thisSuit);
+      } else if (value === 11) {
+          newDeck.push("J" + thisSuit);
+      } else if (value === 12) {
+          newDeck.push("K" + thisSuit);
+      } else if (value === 13) {
+          newDeck.push("Q" + thisSuit);
+      } else if (value === 14) {
+          newDeck.push("A" + thisSuit);
+      }
+    }
+  });
+
+  // shuffle the deck, using "swap" method borrowed from internet
   for (let i = newDeck.length - 1; i > 0; i--) {
-    random_Number = Math.floor(Math.random() * (i + 1));   // 2nd param needs to be inclusive
+    randomNumber = Math.floor(Math.random() * (i + 1));   // 2nd param needs to be inclusive
     temp = newDeck[i];
-    newDeck[i] = newDeck[random_Number];
-    newDeck[random_Number] = temp;
+    newDeck[i] = newDeck[randomNumber];
+    newDeck[randomNumber] = temp;
   }
+
   return newDeck;
-};
+}
--- src/game.js
+++ src/game.js
@@ -31,26 +31,19 @@ module.exports = function playGame(playerArray) {
     ":" +
     timeStartGame.getMinutes());
 
-  // initialize the "results object" as an array of objects
-  gameResults.players = [];
-  gameResults.players[0] = {};
-  gameResults.players[1] = {};
-  gameResults.players[0].name = playerArray[0];
-  gameResults.players[1].name = playerArray[1];
-  gameResults.players[0].numberOfWins = 0;
-  gameResults.players[1].numberOfWins = 0;
-  gameResults.players[0].winRatio = 0;
-  gameResults.players[1].winRatio = 0;
+  // initialize the "results object"
+  gameResults.players =
+  [ { name: playerArray[0],
+      numberOfWins: 0,
+      winRatio: 0 },
+     { name: playerArray[1],
+      numberOfWins: 0,
+      winRatio: 0 } ];
+
   gameResults.numberOfTies = 0;
 
   // pick pairs of cards (that are already shuffled)
-  let card1 = [];
-  let card2 = [];
-
-  // print the contents of the array
-  for (let i = gameDeck.length; i > 0; i--) {
-
-  }
+  let card1, card2;
 
   // draw two cards, compare, and tally the score
   for (let i = gameDeck.length / 2; i > 0; i--) {
@@ -59,170 +52,28 @@ module.exports = function playGame(playerArray) {
     card2 = gameDeck.splice(0, 1);
 
     // convert the cards into numerical values
-    switch (card1[0]) {
-      case "2s":
-      case "2c":
-      case "2h":
-      case "2d":
-        card1[0] = 2;
-        break;
-      case "3s":
-      case "3c":
-      case "3h":
-      case "3d":
-        card1[0] = 3;
-        break;
-      case "4s":
-      case "4c":
-      case "4h":
-      case "4d":
-        card1[0] = 4;
-        break;
-      case "5s":
-      case "5c":
-      case "5h":
-      case "5d":
-        card1[0] = 5;
-        break;
-      case "6s":
-      case "6c":
-      case "6h":
-      case "6d":
-        card1[0] = 6;
-        break;
-      case "7s":
-      case "7c":
-      case "7h":
-      case "7d":
-        card1[0] = 7;
-        break;
-      case "8s":
-      case "8c":
-      case "8h":
-      case "8d":
-        card1[0] = 8;
-        break;
-      case "9s":
-      case "9c":
-      case "9h":
-      case "9d":
-        card1[0] = 9;
-        break;
-      case "10s":
-      case "10c":
-      case "10h":
-      case "10d":
-        card1[0] = 10;
-        break;
-      case "Js":
-      case "Jc":
-      case "Jh":
-      case "Jd":
-        card1[0] = 11;
-        break;
-      case "Qs":
-      case "Qc":
-      case "Qh":
-      case "Qd":
-        card1[0] = 12;
-        break;
-      case "Ks":
-      case "Kc":
-      case "Kh":
-      case "Kd":
-        card1[0] = 13;
-        break;
-      case "As":
-      case "Ac":
-      case "Ah":
-      case "Ad":
-        card1[0] = 14;
-        break;
-      default:
-        break;
+    if (card1[0].charAt(0) === "J") {
+      card1[0] = 11;
+    } else if (card1[0].charAt(0) === "Q") {
+      card1[0] = 12;
+    } else if (card1[0].charAt(0) === "K") {
+      card1[0] = 13;
+    } else if (card1[0].charAt(0) === "A") {
+      card1[0] = 14;
+    } else {
+      card1[0] = parseInt(card1[0]);
     }
 
-    switch (card2[0]) {
-      case "2s":
-      case "2c":
-      case "2h":
-      case "2d":
-        card2[0] = 2;
-        break;
-      case "3s":
-      case "3c":
-      case "3h":
-      case "3d":
-        card2[0] = 3;
-        break;
-      case "4s":
-      case "4c":
-      case "4h":
-      case "4d":
-        card2[0] = 4;
-        break;
-      case "5s":
-      case "5c":
-      case "5h":
-      case "5d":
-        card2[0] = 5;
-        break;
-      case "6s":
-      case "6c":
-      case "6h":
-      case "6d":
-        card2[0] = 6;
-        break;
-      case "7s":
-      case "7c":
-      case "7h":
-      case "7d":
-        card2[0] = 7;
-        break;
-      case "8s":
-      case "8c":
-      case "8h":
-      case "8d":
-        card2[0] = 8;
-        break;
-      case "9s":
-      case "9c":
-      case "9h":
-      case "9d":
-        card2[0] = 9;
-        break;
-      case "10s":
-      case "10c":
-      case "10h":
-      case "10d":
-        card2[0] = 10;
-        break;
-      case "Js":
-      case "Jc":
-      case "Jh":
-      case "Jd":
-        card2[0] = 11;
-        break;
-      case "Qs":
-      case "Qc":
-      case "Qh":
-      case "Qd":
-        card2[0] = 12;
-        break;
-      case "Ks":
-      case "Kc":
-      case "Kh":
-      case "Kd":
-        card2[0] = 13;
-        break;
-      case "As":
-      case "Ac":
-      case "Ah":
-      case "Ad":
-        card2[0] = 14;
-        break;
-      default:
-        break;
+    if (card2[0].charAt(0) === "J") {
+      card2[0] = 11;
+    } else if (card2[0].charAt(0) === "Q") {
+      card2[0] = 12;
+    } else if (card2[0].charAt(0) === "K") {
+      card2[0] = 13;
+    } else if (card2[0].charAt(0) === "A") {
+      card2[0] = 14;
+    } else {
+      card2[0] = parseInt(card2[0]);
     }
 
     // compare the pair of cards and tally the score
