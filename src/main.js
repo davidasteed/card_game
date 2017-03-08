3870c31390996417beccdd3b57b217b4042a3482
David Steed <davidasteed@gmail.com>
2017-03-07 20:59:37 -0500

    Epic mode submitted using process.argv for player inputs.  Wasn't able to find a clean and coherent explanation online for process.stdin after some searching and testing

--- src/main.js
+++ src/main.js
@@ -2,10 +2,28 @@
 // things in here, but once you have created the game correctly, this file
 // should run as is.
 
+// epic mode =  accept user input:
+// extract and store the command line options, if any.
+let players = [];
+if (process.argv[2]) {
+   players[0] = process.argv[2];
+} else {
+    console.log("Please enter a name for player 1");
+  return;
+}
+
+if (process.argv[3]) {
+  players[1] = process.argv[3];
+} else {
+    console.log("Please enter a name for player 2");
+  return;
+}
+
+// launch game with the player names provided
 let game = require('./game.js');
+let results = game(players);
 
-let results = game(['Jordan', 'Russell']);
-
+// output results
 console.log(`A game of War played ${results.date} between ${results.players[0].name} and ${results.players[1].name}...`);
 console.log(
     `It appears that ${results.players[0].name} won ${results.players[0].numberOfWins} times
